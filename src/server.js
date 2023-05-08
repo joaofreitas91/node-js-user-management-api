5a4c29b35e34d409c93ff096c10125d96782f1a7
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-05-07 21:50:49 -0300

    feat: create and list users

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import http from 'node:http'
 
 const port = 3333
 
+const users = []
+
 const server = http.createServer((req, res) => {
     const { url, method } = req
 
     if(url === '/users' && method === 'GET') {
-        return res.end('GET USERS')
+        console.log('GET USERS')
+        return res.end(JSON.stringify(users))
     }
 
     if(url === '/users' && method === 'PUT') {
@@ -14,8 +17,14 @@ const server = http.createServer((req, res) => {
     }
 
     if(url === '/users' && method === 'POST') {
-        console.log('POST USERS')
-        return res.end('POST USERS')
+
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@email.com'
+        })
+        
+        return res.end('User created')
     }
 
     res.end('Hello World')
@@ -23,4 +32,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port , () => {
     console.log(`Server is listening on port ${port}`)
-})
+}) 
